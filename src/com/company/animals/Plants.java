724a80577bec30cb65c652e3abcad5207d36f1f8
RamzanKi <91493572+RamzanKi@users.noreply.github.com>
2022-05-19 17:26:45 +0300

    new stat & refactor

--- src/com/company/animals/Plants.java
+++ src/com/company/animals/Plants.java
@@ -15,83 +15,63 @@ public class Plants extends Animal{
         this.locIndexi = i;
         this.locIndexj = j;
     }
-
     @Override
     public int getMaxAmountInCell() {
         return this.maxAmountInCell;
     }
-
     @Override
     public double getWeight() {
         return this.weight;
     }
-
     @Override
     public double getSatiety() {
         return 0;
     }
-
     @Override
     public void setSatiety(double satiety) {
-
     }
-
     @Override
     public double getFoodForSatiety() {
         return 0;
     }
-
     @Override
     public int getNumberOfMovesAfterHunger() {
         return 0;
     }
-
     @Override
     public int getStarvationCount() {
         return 0;
     }
-
     @Override
     public void setStarvationCount(int starvationCount) {
-
     }
-
     @Override
     public double getWeightLoss() {
         return 0;
     }
-
     @Override
     public void setWeightLoss(double weightLoss) {
-
     }
-
     public String getIndex() {
         return this.locIndexi +":"+ this.locIndexj;
     }
-
     @Override
     public void eat(Animal animal, List<Animal> animalList) {
     }
-
     @Override
     public Animal reproduce(Animal animal) {
         return null;
     }
-
     @Override
     public void moveDirection() {
     }
-
     @Override
     public void setIndex(int i, int j) {
     }
-
     @Override
     public String toString() {
         return name;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -99,7 +79,6 @@ public class Plants extends Animal{
         Plants plants = (Plants) o;
         return Double.compare(plants.weight, weight) == 0 && maxAmountInCell == plants.maxAmountInCell && locIndexi == plants.locIndexi && locIndexj == plants.locIndexj && name.equals(plants.name);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(name, weight, maxAmountInCell, locIndexi, locIndexj);
